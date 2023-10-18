0e49295aec0a8f94544294d0ba11737ffc4cb057
Sam Young <youngsam@sas.upenn.edu>
2023-10-18 14:19:07 -0700

    100% coverage for flashalgo

--- tests/algorithm/test_flashalgo.py
+++ tests/algorithm/test_flashalgo.py
@@ -42,9 +42,13 @@ def config_dict(rng, num_pmt):
                     'GlobalQE': rng.random()*1.e-4 + 1.e-5,
                     'RecoPECalibFactor': rng.random()*2 + 1,
                     'CCVCorrection': rng.random(num_pmt).tolist(),
-                    'SirenPath': os.path.dirname(os.path.dirname(__file__)) + '/data/siren.ckpt'
+                    'SirenPath': None
                 }
             } 
+    
+@pytest.fixture
+def config_sirenpath():
+    return os.path.dirname(os.path.dirname(__file__)) + '/data/siren.ckpt'
 
 @pytest.fixture
 def fake_photon_library(rng, num_pmt):
@@ -81,57 +85,80 @@ def flashalgo_withconfig(fake_photon_library, detector_specs, config_dict):
     yield FlashAlgo(detector_specs, photon_library=plib, cfg_file=config_dict)
     
 @pytest.fixture
-def flashalgo_matrix(fake_photon_library, detector_specs, config_dict):
+def flashalgo_matrix(fake_photon_library, detector_specs, config_dict, config_sirenpath):
     out = []
-    
-    # with yaml config
-    config_yaml = writable_temp_file(suffix='.yaml')
-    yaml.dump(config_dict, open(config_yaml, 'w'))
-    for cfg_file in [None, config_dict, config_yaml]:
-        for plib in [None, PhotonLibrary(fake_photon_library)]:
-            
-            # edge case
-            if cfg_file is None and plib is None:
-                with pytest.raises(RuntimeError):
-                    FlashAlgo(detector_specs, photon_library=plib, cfg_file=cfg_file)
-                continue
-            
-            # all other cases
-            try:
-                out.append(FlashAlgo(detector_specs, photon_library=plib, cfg_file=cfg_file))
-            except Exception as e:
-                raise RuntimeError(f'Failed to initialize FlashAlgo with config {type(cfg_file)} and plib {type(plib)}') from e
-    yield out
-    os.remove(config_yaml)
         
+    dict_without_sirenpath = config_dict
+    dict_with_sirenpath = config_dict.copy()
+    dict_with_sirenpath['PhotonLibHypothesis']['SirenPath'] = config_sirenpath
+    
+    cfg_files = [None, dict_with_sirenpath, dict_without_sirenpath]
+    plibs = [None, PhotonLibrary(fake_photon_library)]
+    config_params = [(cfg, plib) for cfg in cfg_files for plib in plibs]
+
+    for cfg_file, plib in config_params:
+        # edge case
+        if cfg_file is None and plib is None:
+            with pytest.raises(RuntimeError):
+                FlashAlgo(detector_specs, photon_library=plib, cfg_file=cfg_file)
+            continue
+        
+        if cfg_file and not cfg_file['PhotonLibHypothesis']['SirenPath'] and plib is None:
+            with pytest.raises(RuntimeError):
+                FlashAlgo(detector_specs, photon_library=plib, cfg_file=cfg_file)
+            continue
+        
+        # all other cases
+        try:
+            init_info = (cfg_file.__class__.__name__, plib.__class__.__name__)
+            out.append((FlashAlgo(detector_specs, photon_library=plib, cfg_file=cfg_file), init_info))
+        except Exception as e:
+            raise RuntimeError("Failed to initialize FlashAlgo with config " \
+                              f"'{init_info[0]}' and plib '{init_info[1]}'") \
+            from e
+    yield out
+
 """ --------------------------------- tests -------------------------------- """
+def test_flashalgo_matrix(flashalgo_matrix):
+    # empty because the fixture itself tests initialization
+    pass
+
 def test_flashalgo_config_input(fake_photon_library, detector_specs, config_dict):
-    # tests if configuration is properly loaded from dict or file
+    # tests if configuration is properly loaded from dict or yaml
     plib = PhotonLibrary(fake_photon_library)
     config_yaml = writable_temp_file(suffix='.yaml')
     yaml.dump(config_dict, open(config_yaml, 'w'))
     
     dict_init = FlashAlgo(detector_specs, photon_library=plib, cfg_file=config_dict)
     file_init = FlashAlgo(detector_specs, photon_library=plib, cfg_file=config_yaml)
-    for attr in ['global_qe', 'reco_pe_calib', 'qe_v', 'siren_path']:
+    for attr in ['global_qe', 'reco_pe_calib', 'qe_v']:
         compare = lambda a, b: a == b
-        if isinstance(getattr(dict_init, attr), torch.Tensor):
+        
+        dict_attr = getattr(dict_init, attr)
+        file_attr = getattr(file_init, attr)
+
+        if torch.is_tensor(dict_attr):
             compare = torch.allclose
-        elif isinstance(getattr(dict_init, attr), (list, np.ndarray)):
+        elif isinstance(dict_attr, (list, np.ndarray)):
             compare = np.allclose
 
-        assert compare(getattr(dict_init, attr), getattr(file_init, attr)), \
+        assert compare(dict_attr, file_attr), \
             f'Attribute {attr} does not match between configuration dict and file initialization'
 
     os.remove(config_yaml)
 
 def test_fill_estimate_shape(track, flashalgo_matrix, num_pmt):
-    for flashalgo in flashalgo_matrix:
+    for flashalgo, info in flashalgo_matrix:
         estimate = flashalgo.fill_estimate(track)
-        assert estimate.shape == (num_pmt,)
+        assert estimate.shape == (num_pmt,), \
+            f"Estimate tensor shape mismatch for config (cfg={info[0]}, plib={info[1]})"
+        
+        estimate = flashalgo.fill_estimate(track.cpu().numpy())
+        assert estimate.shape == (num_pmt,), \
+            f"Estimate numpy array shape mismatch for config (cfg={info[0]}, plib={info[1]})"
 
 def test_backward_gradient_shape(track, num_pmt, flashalgo_matrix):
-    for flashalgo in flashalgo_matrix:
+    for flashalgo, info in flashalgo_matrix:
         flashalgo.fill_estimate(track)
         if flashalgo.plib is None:
             with pytest.raises(RuntimeError):
@@ -139,9 +166,11 @@ def test_backward_gradient_shape(track, num_pmt, flashalgo_matrix):
             continue
         
         gradient = flashalgo.backward_gradient(track)
-        assert gradient.shape == (3, num_pmt)
+        assert gradient.shape == (3, num_pmt), \
+            f"Gradient shape mismatch for config (cfg={info[0]}, plib={info[1]})"
 
 def test_NormalizePosition_shape(track, flashalgo_matrix):
-    for flashalgo in flashalgo_matrix:
+    for flashalgo, info in flashalgo_matrix:
         normalized = flashalgo.NormalizePosition(track[:,:3])
-        assert normalized.shape == track[:,:3].shape
+        assert normalized.shape == track[:,:3].shape, \
+            f"Normalized position shape mismatch for config (cfg={info[0]}, plib={info[1]})"
